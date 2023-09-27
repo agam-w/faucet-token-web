6c7726c22cfbfd1cf884fcfdfde5614efc0726ef
agam-w <agam.w11@gmail.com>
2023-09-27 12:42:04 +0700

    Init next tailwind app

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import Header from '@/components/Header'
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+
+      <main className="mx-auto max-w-7xl p-6 lg:px-8">
+        <p className="text-lg font-bold text-blue-500">hello</p>
+      </main>
+    </>
+  )
+}
